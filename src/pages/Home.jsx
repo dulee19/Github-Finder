18664bbc8b88a935f446d21b1f758411d0f2d3c8
dulee19 <adusan53@gmail.com>
2022-02-03 19:01:23 +0100

    UI, Github Token, User List, Spinner, Display Users

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserResults from "../components/users/UserResults";
+
+const Home = () => {
+  return (
+    <>
+      {/* Search Component */}
+      <UserResults />
+    </>
+  );
+};
+
+export default Home;
